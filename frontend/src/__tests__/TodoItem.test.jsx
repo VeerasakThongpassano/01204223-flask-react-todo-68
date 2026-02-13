0957a3f50dcfcff73fc9a47f26e195fa2319d3b1
VEERASAK THONGPASSANO <veerasak.th@ku.th>
2026-02-13 15:46:34 +0700

    add button callback test

--- frontend/src/__tests__/TodoItem.test.jsx
+++ frontend/src/__tests__/TodoItem.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { expect } from 'vitest'
 import TodoItem from '../TodoItem.jsx'
 
@@ -44,5 +45,42 @@ describe('TodoItem', () => {
       <TodoItem todo={todoWithComment} />
     );
     expect(screen.queryByText('No comments')).not.toBeInTheDocument();
+  });
+  it('makes callback to toggleDone when Toggle button is clicked', () => {
+    const onToggleDone = vi.fn();
+    render(
+      <TodoItem 
+       todo={baseTodo} 
+       toggleDone={onToggleDone} />
+    );
+    const button = screen.getByRole('button', { name: /toggle/i });
+    button.click();
+    expect(onToggleDone).toHaveBeenCalledWith(baseTodo.id);
+  });
+  it('makes callback to deleteTodo when delete button is clicked', () => {
+    const onDeleteTodo = vi.fn();
+    render(
+      <TodoItem 
+       todo={baseTodo} 
+       deleteTodo={onDeleteTodo} />
+    );
+    const button = screen.getByRole('button', { name: /❌/i });
+    button.click();
+    expect(onDeleteTodo).toHaveBeenCalledWith(baseTodo.id);
+  });
+  it('makes callback to addNewComment when a new comment is added', async () => {
+    const onAddNewComment = vi.fn();
+    render(
+      <TodoItem 
+       todo={baseTodo} 
+       addNewComment={onAddNewComment} />
+    );
+
+    const input = screen.getByRole('textbox');
+    await userEvent.type(input, 'New comment');
+
+    const button = screen.getByRole('button', { name: /add comment/i });
+    button.click();
+    expect(onAddNewComment).toHaveBeenCalledWith(baseTodo.id, 'New comment');
   });
 });
